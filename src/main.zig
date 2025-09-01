cf0b61f68852f4c099d349a5b4fdddc29b0e6842
cincip <kuatdingazy@gmail.com>
2025-09-01 20:20:30 +0500

    Added speed

--- src/main.zig
+++ src/main.zig
@@ -16,13 +16,30 @@ pub fn main() !void {
 
     rl.initWindow(window_width, window_height, "Game of Life");
     defer rl.closeWindow();
-    rl.setTargetFPS(10);
+    rl.setTargetFPS(60);
 
     var running = false;
+    var speed: f64 = 15;
+    var last_time: f64 = 0;
 
     while (!rl.windowShouldClose()) {
+        const time = rl.getTime();
+        const inv_speed = 1 / speed;
+
         if (rl.isKeyPressed(rl.KeyboardKey.space)) {
             running = !running;
+        }
+
+        if (rl.isKeyPressed(rl.KeyboardKey.up)) {
+            speed += 1;
+        }
+
+        if (rl.isKeyPressed(rl.KeyboardKey.down)) {
+            speed -= 1;
+        }
+
+        if (rl.isKeyPressed(rl.KeyboardKey.r)) {
+            speed = 15;
         }
 
         if (rl.isMouseButtonDown(rl.MouseButton.left)) {
@@ -42,8 +59,9 @@ pub fn main() !void {
             }
         }
 
-        if (running) {
+        if (running and (time - last_time) >= inv_speed) {
             try board.step();
+            last_time = time;
         }
 
         rl.beginDrawing();
@@ -52,5 +70,9 @@ pub fn main() !void {
         rl.clearBackground(.ray_white);
         board.draw(margin);
         rl.drawText(if (running) "Running (SPACE to pause)" else "Paused (SPACE to run)", 10, 10, 20, .dark_gray);
+
+        var speed_buffer: [64]u8 = undefined;
+        const speed_text = try std.fmt.bufPrintZ(&speed_buffer, "Speed: {d:.1} steps/sec (UP and DOWN to adjust, R to reset)", .{speed}); // LOL ChatGPT is the GOAT for this one
+        rl.drawText(@ptrCast(speed_text), 10, 35, 16, .dark_gray);
     }
 }
